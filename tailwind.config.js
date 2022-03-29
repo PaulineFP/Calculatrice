3ca5c9f4a1e5e3805c3ade5aeee91aaa7eb0fc3c
Pauline <pauline.frare-pradal@laposte.net>
2022-03-29 09:44:45 +0200

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+    purge: ['./public/*.html'],
+    darkmode: false,
+    theme: {
+      extend: {},
+    },
+    variants: {
+      extend: {},
+    },
+    plugins: [],
+  };
+  
---
+++ webpack.config.js
@@ -0,0 +1,43 @@
+const path = require("path");
+
+const htmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: "./src/index.js",
+  output: {
+    path: path.resolve(__dirname, "./public"),
+    publicPath: "/asset/",
+    filename: "[name].bundle.js",
+  },
+  mode: 'development',
+  devServer: {
+   // static: {path.join(__dirname, './public')},
+   static: "./public",
+   //compress: true,// active la compression gzip
+  // open: true,
+  // historyApiFallback: true, // true pour index.html sur 404, objet pour plusieurs chemins
+   // hot: true, // hot module replacement. Depends on HotModuleReplacementPlugin
+   // https: false,
+   port: 3000,
+ },
+ module: {
+   rules: [
+     {
+      test: /\.js$/,
+      exclude: /node_modules/,
+      loader: "babel-loader"
+    },
+     {
+       test: /\.(scss|css)$/,
+       use: ["style-loader", "css-loader"],
+     },
+  ],
+ },
+  plugins: [
+    new htmlWebpackPlugin({
+      template: path.resolve(__dirname, './src/index.html'),
+      filename: 'index.html',
+    }),
+
+  ],
+}
